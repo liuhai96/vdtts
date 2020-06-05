18b4c18c145eb5ac8331c1dd58bcabd843dc1cf6
liuhai96 <923751365@qq.com>
2020-06-05 14:40:21 +0800

    Initial commit

---
+++ src/main/java/com/lsjbc/vdtts/VdttsApplication.java
@@ -0,0 +1,13 @@
+package com.lsjbc.vdtts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdttsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VdttsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsjbc/vdtts/VdttsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsjbc.vdtts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdttsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
