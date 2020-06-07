b675b6fd542772bcbb4f1bcaf1b7ae7f48a60c55
liuhai96 <923751365@qq.com>
2020-06-07 23:56:20 +0800

    添加账号类的mapper和接口

---
+++ src/main/java/com/lsjbc/vdtts/service/impl/AccountImpl.java
@@ -0,0 +1,26 @@
+//package com.lsjbc.vdtts.service.impl;
+//
+//import com.lsjbc.vdtts.dao.mapper.AccountMapper;
+//import com.lsjbc.vdtts.entity.Account;
+//import com.lsjbc.vdtts.service.intf.AccountService;
+//import com.lsjbc.vdtts.tools.LayuiData;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AccountImpl implements AccountService {
+//
+//    @Autowired
+//    private AccountMapper accountMapper;
+////    @Override
+// /*   public LayuiData findAccount(String schoolAccount) {
+//        Account account = accountMapper.findAccount(schoolAccount);
+//        LayuiData layuiData = new LayuiData();
+//        if(account==null){
+//
+//        }else{
+//            layuiData.setCode(0);
+//        }
+//        return null;
+//    }*/
+//}
---
+++ src/main/java/com/lsjbc/vdtts/service/intf/AccountService.java
@@ -0,0 +1,8 @@
+package com.lsjbc.vdtts.service.intf;
+
+import com.lsjbc.vdtts.entity.Account;
+import com.lsjbc.vdtts.tools.LayuiData;
+
+public interface AccountService {
+    public LayuiData findAccount(String account);
+}
